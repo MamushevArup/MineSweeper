e8f05437c6842050c36dce1bb21d0494c59d6375
Arup Mamushev <93328884+MamushevArup@users.noreply.github.com>
2023-05-20 01:00:15 +0600

    Creating grid field for game

--- main.go
+++ main.go
@@ -1,10 +1,57 @@
 package main
 
-type Info struct {
-	State    int
-	Position []int
-}
+import (
+	"fmt"
+)
 
 func main() {
+	var (
+		name            string
+		difficultyLevel int
+	)
+	level := []string{"Beginner", "Intermediate", "Expert"}
+	fmt.Print("Enter your name: ")
+	if _, err := fmt.Scan(&name); err != nil {
+		return
+	}
+	for i := 0; i < len(level); i++ {
+		fmt.Print(i+1, " -> "+level[i]+"\n")
+	}
+	fmt.Print("Enter the difficulty: ")
+	if _, err := fmt.Scan(&difficultyLevel); err != nil {
+		return
+	}
+	showField(createField(15, 15))
+}
+
+const ROWS, MINE = 8, 10
+
+func createField(rows int, column int) [][]string {
+	arr := make([][]string, rows)
+	for i := 0; i < rows; i++ {
+		arr[i] = make([]string, column)
+	}
+	for i := 0; i < rows; i++ {
+		for j := 0; j < column; j++ {
+			arr[i][j] = "+"
+		}
+	}
+	return arr
+}
+func showField(arr [][]string) {
+	fmt.Print("  ")
+	for j := 1; j <= len(arr[0]); j++ {
+		fmt.Printf("%2d ", j)
+	}
+	fmt.Println()
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%2d ", i+1)
+		for j := 0; j < len(arr[i]); j++ {
+			fmt.Print(arr[i][j], "  ")
+		}
+		fmt.Println()
+	}
+}
+func startGame() {
 
 }
